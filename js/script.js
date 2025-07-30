e8fa4f4216a34a4b3be142bb1293775487bd69f0
iflocky <Petrenko1702@gmail.com>
2025-07-30 11:37:21 +0300

    feat: add styles for adaptive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const range = document.querySelector("input[type='range']");
-  const label = range?.nextElementSibling;
+  
+  // Burger
+  const burger = document.querySelector(".header__burger");
+  const nav = document.querySelector(".header__nav");
 
-  if (range && label) {
-    label.textContent = `${range.value} %`;
+  const closeMenu = () => {
+    nav.classList.remove("is-open");
+    burger.classList.remove("is-active");
+  };
+
+  burger.addEventListener("click", (e) => {
+    e.stopPropagation();
+    nav.classList.toggle("is-open");
+    burger.classList.toggle("is-active");
+  });
+
+  document.addEventListener("click", (e) => {
+    const isClickInsideMenu = nav.contains(e.target);
+    const isClickOnBurger = burger.contains(e.target);
+
+    if (!isClickInsideMenu && !isClickOnBurger) {
+      closeMenu();
+    }
+  });
+
+
+  // Range 
+  const range = document.querySelector("input[type='range']");
+  const label = range?.closest("label");
+  const percentSpan = label?.querySelector("div span:last-child"); 
+
+  if (range && percentSpan) {
+    percentSpan.textContent = `${range.value} %`;
 
     range.addEventListener("input", () => {
-      label.textContent = `${range.value} %`;
+      percentSpan.textContent = `${range.value} %`;
     });
   }
+
 });
